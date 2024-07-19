4d75357eb1953b333729a3c7d7cdc29fd850948f
pandurx <internalfire90@gmail.com>
2024-07-18 23:35:29 -0400

    initial

---
+++ Infrastructure/Database/NPocoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Database
+{
+    public class NPocoDbContext : INPocoDbContext
+    {
+        public void AddRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestEntity GetRequestId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
